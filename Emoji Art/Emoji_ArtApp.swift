8ab5e5cb45b12f314a130fe41fa23b513ea49583
Rishi Jansari <jansaririshi@gmail.com>
2024-07-15 15:23:52 +0100

    Initial Commit

---
+++ Emoji Art/Emoji_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_ArtApp.swift
+//  Emoji Art
+//
+//  Created by Rishi Jansari on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
